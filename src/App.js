69b2b519d0ad09697ee42d3a47139f91cd58058d
aswin kumar alapati <aswinalapati1@gmail.com>
2020-08-02 08:05:37 -0400

    add config overrides, logo, container div

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React from 'react';
 import './App.css';
+import ReactLogo from './logo.svg';
 
 function App() {
+  const imgSrc = `http://localhost:5002${ReactLogo}`
   return (
     <div className="App">
+      <img src={imgSrc} className="App-logo" alt="react logo" />
       <h3>Hi from React Micro Front End App</h3>
     </div>
   );
