e9eb8b41bf0a27739ba91b215f548c2af70aafbb
aswin kumar alapati <aswinalapati1@gmail.com>
2020-08-05 17:51:40 -0400

    add window event listener

--- src/App.js
+++ src/App.js
@@ -1,13 +1,33 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import ReactLogo from './logo.svg';
 
 function App() {
-  const imgSrc = `http://localhost:5002${ReactLogo}`
+  const [items, setItems] = useState([]);
+  const imgSrc = `http://localhost:5002${ReactLogo}`;
+
+  useEffect(() => {
+    window.addEventListener('addToCart', (event) => {
+      setItems({ items: [...items, event.product] });
+    }, false);
+  });
+
+  const renderCart = () => {
+    if (items.length === 0) {
+      return <p>Your cart is empty</p>;
+    }
+    return <ul>{ items.map(renderItem) }</ul>;
+  }
+
+  const renderItem = (product, index) => {
+    return <li key={index}>{ product.name } - ${ product.price }</li>;
+  }
+
   return (
     <div className="App">
       <img src={imgSrc} className="App-logo" alt="react logo" />
-      <h3>Hi from React Micro Front End App</h3>
+      <h3>Cart</h3>
+      { renderCart() }
     </div>
   );
 }
