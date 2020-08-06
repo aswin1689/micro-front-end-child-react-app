a0f72ca9428c4a9cc76eb10ef528154814ad39a4
aswin kumar alapati <aswinalapati1@gmail.com>
2020-08-06 19:54:00 -0400

    add library externals config

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,8 @@
 module.exports = function override(config, env) {
-    //do stuff with the webpack config...
+    config.externals = {
+        'react': 'React',
+        'react-dom': 'ReactDOM'
+    }
+    
     return config;
 }
